f946a60ef7054cb8569ecff1ed59edf1478c3b7e
SwarajXO <purarkarswaraj@gmail.com>
2025-11-24 14:08:32 +0530

    Setup

---
+++ src/main/java/com/swaraj/url_shortener/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.swaraj.url_shortener.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
