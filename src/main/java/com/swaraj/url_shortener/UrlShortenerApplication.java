cca65feda02d1a41cdd22f94a1862df424e9791a
SwarajXO <purarkarswaraj@gmail.com>
2025-11-24 14:19:22 +0530

    Health checkup is done

--- src/main/java/com/swaraj/url_shortener/UrlShortenerApplication.java
+++ src/main/java/com/swaraj/url_shortener/UrlShortenerApplication.java
@@ -2,11 +2,11 @@ package com.swaraj.url_shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
-})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class UrlShortenerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
 	}
--- src/main/java/com/swaraj/url_shortener/controller/HealthCheckController.java
+++ src/main/java/com/swaraj/url_shortener/controller/HealthCheckController.java
@@ -8,6 +8,6 @@ public class HealthCheckController {
 
     @GetMapping("/health")
     public String health() {
-        return "OK";
+        return "Swaraj is checking Health";
     }
 }
