9ee3a0b33eb9210769c9b2f7e7abb2872f98fa3a
SwarajXO <purarkarswaraj@gmail.com>
2025-11-24 14:11:06 +0530

    initial commit

--- src/main/java/com/swaraj/url_shortener/UrlShortenerApplication.java
+++ src/main/java/com/swaraj/url_shortener/UrlShortenerApplication.java
@@ -3,11 +3,11 @@ package com.swaraj.url_shortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
 public class UrlShortenerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
 	}
-
 }
