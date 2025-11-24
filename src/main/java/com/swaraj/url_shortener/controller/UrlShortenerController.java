4a44a588853cd5025e76dc76397208fea543e9f2
SwarajXO <purarkarswaraj@gmail.com>
2025-11-24 15:11:15 +0530

    able to see shorten url by doing post method

--- src/main/java/com/swaraj/url_shortener/controller/UrlShortenerController.java
+++ src/main/java/com/swaraj/url_shortener/controller/UrlShortenerController.java
@@ -4,9 +4,13 @@ import com.swaraj.url_shortener.model.ShortUrl;
 import com.swaraj.url_shortener.repository.ShortUrlRepository;
 import com.swaraj.url_shortener.service.UrlShortenerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestController
 public class UrlShortenerController {
@@ -16,6 +20,22 @@ public class UrlShortenerController {
 
     @Autowired
     private ShortUrlRepository shortUrlRepository;
+
+    @GetMapping("/{code}")
+    public ResponseEntity<Void> redirect(@PathVariable String code) {
+
+        Optional<ShortUrl> record = shortUrlRepository.findByShortCode(code);
+
+        if (record.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .location(URI.create(record.get().getLongUrl()))
+                .build();
+    }
+
 
     @PostMapping("/shorten")
     public String shorten(@RequestParam String longUrl) {
